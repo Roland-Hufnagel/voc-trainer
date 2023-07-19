d38170c8709514a11adfcbb83c59b4cc170c4e2c
Roland Hufnagel <111574974+Roland-Hufnagel@users.noreply.github.com>
2023-07-19 10:25:17 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+
+export default function Home() {
+  return (
+    <main>
+      <Heading>🏄‍♂️🏄‍♀️🏄 Capstone Template 🏄🏄‍♀️🏄‍♂️</Heading>
+      <SubHeading>Happy hacking!</SubHeading>
+    </main>
+  );
+}

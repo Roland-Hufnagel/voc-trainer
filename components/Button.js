3a6da9add26fa082d4765b06b22e7a61da5b6ce6
Kai Franke <cosinushh@gmail.com>
2023-07-28 00:15:37 +0200

    style 'Button' component

--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
-const Button = styled.button``;
+const Button = styled.button`
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 5px;
+    background-color: gold;
+`;
 
 export { Button };

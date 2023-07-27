554077300a0de92398e9a7a38baa115273a2d2b9
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-07-27 19:56:12 +0200

    add useState and useEffect

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,41 @@ import { vocs } from "../lib/db";
 import Card from "../components/Card";
 import styled from "styled-components";
 import Cardlist from "../components/Cardlist";
+import { useState, useEffect } from "react";
 
+function getXRandomIndexes(x, max) {// max ist maximaler Index!
+  if (max < 0) {
+    alert("Keine Vokabeln vorhanden!");
+  }
+  if (max < x) {
+    x = max - 1;
+  }
 
-
-
+  const arr = [];
+  while (arr.length <= x) {
+    const randomNumber = Math.floor(Math.random() * (max + 1));
+    if (!arr.includes(randomNumber)) {
+      arr.push(randomNumber);
+    }
+  }
+  console.log(arr);
+  return arr;
+}
 
 export default function Home() {
+  const [cards, setCards] = useState(vocs);
+  const [indexes, setIndexes] = useState([]);
+  useEffect(() => {
+    setIndexes(getXRandomIndexes(3, cards.length - 1));
+  }, []);
+
   return (
     <>
       <Header />
       <StyledMain>
         <Cardlist>
-          {vocs.map((voc) => {
-            return <Card key={voc.id} voc={voc}></Card>;
+          {indexes.map((item) => {
+            return <Card key={item} voc={cards[item]}></Card>;
           })}
         </Cardlist>
       </StyledMain>

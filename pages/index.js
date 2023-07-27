9763c9ec2c2881c67d6628392c27201d16792293
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-07-27 20:52:30 +0200

    conditional rendering

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,21 @@ import styled from "styled-components";
 import Cardlist from "../components/Cardlist";
 import { useState, useEffect } from "react";
 
-function getXRandomIndexes(x, max) {// max ist maximaler Index!
-  if (max < 0) {
-    alert("Keine Vokabeln vorhanden!");
-  }
-  if (max < x) {
-    x = max - 1;
-  }
+function getXRandomIndexes(x, length) {
+  // x => how many cards you want
+  // length => how many cards there are
 
+  if (x > length) {
+    x = length;
+  }
+  const maxIndex = length - 1;
   const arr = [];
-  while (arr.length <= x) {
-    const randomNumber = Math.floor(Math.random() * (max + 1));
+  while (arr.length < x) {
+    const randomNumber = Math.floor(Math.random() * (maxIndex + 1));
     if (!arr.includes(randomNumber)) {
       arr.push(randomNumber);
     }
   }
-  console.log(arr);
   return arr;
 }
 
@@ -28,18 +27,22 @@ export default function Home() {
   const [cards, setCards] = useState(vocs);
   const [indexes, setIndexes] = useState([]);
   useEffect(() => {
-    setIndexes(getXRandomIndexes(3, cards.length - 1));
+    setIndexes(getXRandomIndexes(5, cards.length));
   }, []);
 
   return (
     <>
       <Header />
       <StyledMain>
-        <Cardlist>
-          {indexes.map((item) => {
-            return <Card key={item} voc={cards[item]}></Card>;
-          })}
-        </Cardlist>
+        {cards.length > 0 ? (
+          <Cardlist>
+            {indexes.map((item) => {
+              return <Card key={item} voc={cards[item]}></Card>;
+            })}
+          </Cardlist>
+        ) : (
+          <p>Keine Vokabeln vorhanden</p>
+        )}
       </StyledMain>
     </>
   );

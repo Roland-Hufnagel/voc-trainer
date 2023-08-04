aed90bc1a48928fd342221569d3a2708edd5ee7e
Kai Franke <cosinushh@gmail.com>
2023-08-04 10:26:03 +0200

    change background-color of <body>

--- styles.js
+++ styles.js
@@ -1,12 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  :root {
+    --white: #ffffff;
+    --darktext: #27273a;
+    --background: #f5f7fd;
+    
+    font-size: 100%;     
+      }
+
   *,
   *::before,
   *::after {
     box-sizing: border-box;
   }
+
   body {
+    background-color: var(--background);
     margin: 0;
     font-family: system-ui;
   }

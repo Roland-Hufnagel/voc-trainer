6f83eaa93d078d9b69aff6f97fa4baea4e4f5a4f
Kai Franke <cosinushh@gmail.com>
2023-09-10 17:52:04 +0200

    display setting's data on 'dashboard' page

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,3 +1,35 @@
-export default function Dashboard() {
-  return <h2>Dashboard</h2>;
+import styled from "styled-components";
+
+export default function Dashboard({ settings }) {
+  return (
+    <>
+      <h2>Settings</h2>
+
+      <SettingsList>
+        {settings.map(({ name, label, value }) => {
+          return (
+            <SettingCard key={name}>
+              <p>{label}</p>
+              <p>{value}</p>
+            </SettingCard>
+          );
+        })}
+      </SettingsList>
+    </>
+  );
 }
+
+const SettingsList = styled.ul`
+  all: unset;
+`;
+
+const SettingCard = styled.li`
+  background-color: var(--white);
+  margin: 1rem auto;
+  padding: 0.8rem;
+  width: 30rem;
+  border-radius: 0.25rem;
+  box-shadow: var(--boxshadow-secondary);
+  display: flex;
+  gap: 1rem;
+`;

737cad9e4afcae0b7dfc6e9a2ec8478e6dac368e
Kai Franke <cosinushh@gmail.com>
2023-09-17 09:57:29 +0200

    refactor css of 'Button' component in order to make 'large' option work

--- components/Button.js
+++ components/Button.js
@@ -1,43 +1,59 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export default function Button({ text, onClick, icon, large, primary }) {
-  const Icon = icon;
-  const iconSize = large ? "40" : "20";
+const SMALL_ICON_SIZE = "20";
+const LARGE_ICON_SIZE = "28";
+
+export default function Button({ text, onClick, icon: Icon, large }) {
+  const iconSize = large ? LARGE_ICON_SIZE : SMALL_ICON_SIZE;
 
   return (
-    <StyledButton onClick={onClick} primary={primary}>
-      {icon && <Icon width={iconSize} />}
+    <StyledButton onClick={onClick} large={large}>
+      {Icon && <Icon width={iconSize} />}
       <span>{text}</span>
     </StyledButton>
   );
 }
 
+const buttonStyles = css`
+  font-size: 0.9rem;
+  padding: 0.35rem 0.75rem;
+  border-width: 2px;
+  gap: 0.5rem;
+`;
+
+const largeButtonStyles = css`
+  font-size: 1.3rem;
+  padding: 0.7rem 1.3rem;
+  border-width: 2px;
+  gap: 0.7rem;
+`;
+
 const StyledButton = styled.button`
   all: unset;
   cursor: pointer;
   display: flex;
-  gap: 0.5rem;
-  border: 2px solid var(--link-active);
+  align-items: center;
+  border: solid var(--button);
   border-radius: 5px;
-  padding: 0.75rem;
   background-color: var(--white);
-  color: var(--link-active);
-  font-size: 0.9rem;
+  color: var(--button);
   transition: 0.3s ease;
 
   svg {
-    fill: var(--link-active);
-    transition: fill 0.3s ease;
+    fill: var(--button);
+    transition: 0.3s ease;
   }
 
   &:hover {
-    color: var(--link-inactive);
-    border: 2px solid var(--link-inactive);
+    color: var(--button-hover);
+    border: 2px solid var(--button-hover);
   }
 
   &:hover svg {
-    fill: var(--link-inactive);
+    fill: var(--button-hover);
   }
+
+  ${(props) => (props.large ? largeButtonStyles : buttonStyles)}
 `;
 
 export { Button };

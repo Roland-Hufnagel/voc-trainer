a3213b45d76ea27baa99b80981303c425a82bd36
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-09-25 11:06:50 +0200

    move initial settings to /lib/db

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,9 @@ import GlobalStyle from "../styles";
 import Head from "next/head";
 
 import useLocalStorageState from "use-local-storage-state";
-import { vocs } from "../lib/db";
+import { vocs, initialSettings } from "../lib/db";
 
 import Layout from "../components/Layout";
-
-const initialSettings = [
-  {
-    name: "numCardsToPick",
-    label: "Cards per game:",
-    value: 5,
-  },
-  {
-    name: "hitsToWin",
-    label: "Iterations to win:",
-    value: 3,
-  },
-];
 
 export default function App({ Component, pageProps }) {
   const [cards, setCards] = useLocalStorageState("cards", {

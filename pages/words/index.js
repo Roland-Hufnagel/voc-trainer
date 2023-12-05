85a0dd56375bba1b4320b54e2886afecf900de67
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-12-05 10:50:51 +0100

    implement fuse.js for fuzzy search in wordspage

--- pages/words/index.js
+++ pages/words/index.js
@@ -1,21 +1,56 @@
+import { useState } from "react";
 import WordsCard from "../../components/WordsCard";
-import EditWordForm from "../../components/EditWordForm";
+import Fuse from "fuse.js";
+import styled from "styled-components";
 
 export default function WordsPage({
   cards,
   handleChangeCard,
   handleDeleteCard,
 }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fuseOptions = {
+    keys: ["word", "translation"],
+    threshold: 0.3,
+  };
+
+  const fuse = new Fuse(cards, fuseOptions);
+  const results = fuse.search(searchTerm);
+
+  const filteredCards = searchTerm
+    ? results.map((result) => result.item)
+    : cards;
+
   return (
-    <ul>
-      {cards.map((card) => (
-        <WordsCard
-          key={card.id}
-          card={card}
-          handleChangeCard={handleChangeCard}
-          handleDeleteCard={handleDeleteCard}
-        />
-      ))}
-    </ul>
+    <Wrapper>
+      <StyledInput
+        type="text"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        placeholder="search..."
+      />
+      <ul>
+        {filteredCards.map((card) => (
+          <WordsCard
+            key={card.id}
+            card={card}
+            handleChangeCard={handleChangeCard}
+            handleDeleteCard={handleDeleteCard}
+          />
+        ))}
+      </ul>
+    </Wrapper>
   );
 }
+const StyledInput = styled.input`
+  line-height: 2rem;
+  border-radius: 0.5rem;
+  margin: 0.2rem;
+  padding: 0.2rem 0.4rem;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;

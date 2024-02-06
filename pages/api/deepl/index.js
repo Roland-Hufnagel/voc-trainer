e3d11d93cf7fa41decf438f3faf05db16ae3645b
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-02-06 16:53:59 +0100

    integrate deeple API route

---
+++ pages/api/deepl/index.js
@@ -0,0 +1,9 @@
+import * as deepl from "deepl-node";
+
+const authKey = process.env.DEEPLE_API_KEY;
+const translator = new deepl.Translator(authKey);
+
+export default async function handler(req, res) {
+  const result = await translator.translateText(req.body, null, "de");
+  res.status(200).json(result.text);
+}

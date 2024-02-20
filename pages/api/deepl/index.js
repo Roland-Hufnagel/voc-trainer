c019c4cb0c021bd59fe5befbfb8f21ad4ad6c19f
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-02-20 18:31:32 +0100

    add lowerCase to API

--- pages/api/deepl/index.js
+++ pages/api/deepl/index.js
@@ -4,6 +4,6 @@ const authKey = process.env.DEEPL_API_KEY;
 const translator = new deepl.Translator(authKey);
 
 export default async function handler(request, response) {
-  const result = await translator.translateText(request.body, "en", "de");
+  const result = await translator.translateText(request.body.toLowerCase(), "en", "de");
   response.status(200).json(result.text);
 }

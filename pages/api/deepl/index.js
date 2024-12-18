686c29e401cb2c7986e265f06e9e82990d6ab5aa
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-12-18 13:35:40 +0100

    add console log for test in api route

--- pages/api/deepl/index.js
+++ pages/api/deepl/index.js
@@ -4,6 +4,11 @@ const authKey = process.env.DEEPL_API_KEY;
 const translator = new deepl.Translator(authKey);
 
 export default async function handler(request, response) {
-  const result = await translator.translateText(request.body.toLowerCase(), "en", "de");
+  console.log("in api route");
+  const result = await translator.translateText(
+    request.body.toLowerCase(),
+    "en",
+    "de"
+  );
   response.status(200).json(result.text);
 }

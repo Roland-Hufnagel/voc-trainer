6d9d34150ff84a178f52efa83ea85ba9c119c3e5
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-12-18 13:37:49 +0100

    change console. log

--- pages/api/deepl/index.js
+++ pages/api/deepl/index.js
@@ -4,7 +4,7 @@ const authKey = process.env.DEEPL_API_KEY;
 const translator = new deepl.Translator(authKey);
 
 export default async function handler(request, response) {
-  console.log("in api route");
+  console.log("in api route", authKey);
   const result = await translator.translateText(
     request.body.toLowerCase(),
     "en",
